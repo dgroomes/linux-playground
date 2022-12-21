85f2a603ae0e48d03a7741d65d7040d88bcaf1e1
David Groomes <dwgroomes@gmail.com>
2022-12-21 21:40:50 +0000

    Use the official Debian image and opt-out of the other one

--- ssh-container-attach.sh
+++ ssh-container-attach.sh
@@ -10,10 +10,13 @@
 # The behavior and techniques relating to running detached interactive Docker containers, attaching to them, and detaching
 # from them is well-discussed in discussions like this StackOverflow question and answers: https://stackoverflow.com/q/25267372
 #
-# Our workaround is to define a special detach sequence. Arbitrarily, let's decide that typing 'exit@@@'' will detach us
+# Our workaround is to define a special detach sequence. Arbitrarily, let's decide that typing 'xit@@@'' will detach us
 # from the container. See https://docs.docker.com/engine/reference/commandline/attach/#override-the-detach-sequence
+#
+# Starting with an 'x' instead of the letter 'e' which is the proper spelling of the word 'exit' is useful because 'x'
+# is a much less frequently used letter so you won't run into the "key presses are invisible" issue as frequently.
 #
 # Admittedly, this is an unusual scenario but the ability to attach/detach from a container is important for enabling a
 # convenient and exploratory development environment which is critical for "playground"-style repositories like 'linux-playground'.
 # The workaround is worth it. Be sure to document your quirky workarounds!
-docker attach --detach-keys='e,x,i,t,@,@,@' ssh-server
+docker attach --detach-keys='x,i,t,@,@,@' ssh-server
--- ssh-container-start.sh
+++ ssh-container-start.sh
@@ -3,8 +3,16 @@
 # Start the 'SSH server' Docker container.
 #
 # We will use this Docker container to install SSH server software (OpenSSH), start an SSH server and connect to it
-# remotely via an SSH client. Note: because our development environment is already in a container context because we're
-# using "Dev Containers", this is a "Docker-in-Docker" scenario. Things can get confusing when doing Docker-in-Docker.
+# remotely via an SSH client from the main dev container. Note: because our development environment is already in a
+# container context because we're using "Dev Containers", this is a "Docker-in-Docker" scenario. Things can get confusing
+# when doing Docker-in-Docker.
+
+# Let's use an official Debian Docker image to keep things as Linux-native as feasible. Let's pin to a major version
+# by its code word instead of using the "latest" tag because it's a moving target. As time goes by, consider the need
+# to upgrade to the latest stable release of Debian.
+#
+# https://hub.docker.com/_/debian
+IMAGE=debian:bullseye
 
 set -eu
 
@@ -14,4 +22,4 @@ docker container run \
     --interactive \
     --tty \
     --detach \
-    mcr.microsoft.com/devcontainers/base:bullseye
+    "$IMAGE"
